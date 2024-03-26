d330e4a5700e08b92c890ecbc58d6ce116e0469b
Jonas <r0831281@student.thomasmore.be>
2024-03-26 19:08:59 +0100

    model fixes

--- inventoryAPP/models.py
+++ inventoryAPP/models.py
@@ -37,7 +37,7 @@ class Bestelling(models.Model):
     date = models.DateField()
     BestellingLocation = models.ForeignKey('Location', on_delete=models.SET_NULL, null=True)
     def __str__(self):
-        return self.name
+        return self.id
     
 class Location(models.Model):
     id = models.AutoField(primary_key=True)
@@ -89,6 +89,6 @@ class userItem(models.Model):
     user = models.ForeignKey('User', on_delete=models.PROTECT, null=True)
     item = models.ForeignKey('Item', on_delete=models.PROTECT, null=True)
     def __str__(self):
-        return self.name
+        return self.id
 
 
